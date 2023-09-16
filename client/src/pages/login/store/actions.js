5e4c2d9dca68c9492f02cfdd5bf8a4892a5febd2
Your Name <you@example.com>
2023-09-16 06:34:30 -0700

    v3

--- client/src/pages/login/store/actions.js
+++ client/src/pages/login/store/actions.js
@@ -1,2 +1,14 @@
-import {STORE_USER, REMOVE_USER} form "./types";
+import { STORE_USER, REMOVE_USER } form "./types";
 
+export const setUser = () => {
+    return {
+        type: STORE_USER,
+        payload: user
+    }
+}
+
+export const removeUser = () => {
+    return {
+        type: REMOVE_USER
+    }
+}
